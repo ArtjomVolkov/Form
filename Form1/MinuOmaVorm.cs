09e99cc537f07dde4a92af9273cfa8f5ef4de443
opilane <opilane@HP-CZC6297B82>
2022-09-20 14:56:48 +0300

    githubd

--- Form1/MinuOmaVorm.cs
+++ Form1/MinuOmaVorm.cs
@@ -15,6 +15,12 @@ namespace Form1
         TreeView puu;
         Button nupp;
         Label silt;
+        CheckBox mruut1, mruut2;
+        ProgressBar riba;
+        RadioButton rnupp1,rnupp2,rnupp3,rnupp4;
+        PictureBox pilt;
+        Button button4;
+        Timer aeg;
         public MinuOmaVorm()
         {
             Height = 600; //высота
@@ -28,20 +34,44 @@ namespace Form1
             oksad.Nodes.Add(new TreeNode("Nupp-Button"));
             oksad.Nodes.Add(new TreeNode("Silt-Label"));
             oksad.Nodes.Add(new TreeNode("Dialog aken-MessageBox"));
+            oksad.Nodes.Add(new TreeNode("CheckBox"));
+            oksad.Nodes.Add(new TreeNode("Radionupp-Radiobutton"));
+            oksad.Nodes.Add(new TreeNode("ProgressBar"));
+            oksad.Nodes.Add(new TreeNode("Mouse-button"));
             puu.AfterSelect += Puu_AfterSelect;
             puu.Nodes.Add(oksad);
             this.Controls.Add(puu);
         }
-
+        
         private void Puu_AfterSelect(object sender,TreeViewEventArgs e)
         {
-            if(e.Node.Text== "Nupp-Button")
+            silt = new Label
+            {
+                Text = "Minu oma vorm koos elementidega",
+                Size = new Size(200, 50),
+                Location = new Point(200, 0)
+            };
+            mruut1 = new CheckBox
+            {
+                Checked = false,
+                Text = "TARpv21",
+                Location = new Point(silt.Left + silt.Width, 30),
+                Height = 50
+            };
+            mruut2 = new CheckBox
+            {
+                Checked = false,
+                Text = "LOGITpv21",
+                Location = new Point(silt.Left + silt.Width, 75),
+                Height = 50
+            };
+            if (e.Node.Text== "Nupp-Button")
             {
                 nupp = new Button();
                 nupp.Text = "Valjuta siia";
                 nupp.Height = 30;
                 nupp.Width = 100;
-                nupp.Location = new Point(340, 100);
+                nupp.Location = new Point(340, 150);
                 nupp.Click += Nupp_Click;
                 this.Controls.Add(nupp);
                 nupp.BackColor = Color.Red;
@@ -80,7 +110,191 @@ namespace Form1
                 }
 
             }
+            else if (e.Node.Text=="CheckBox")
+            {
+                
+                mruut1.CheckedChanged += new EventHandler(Mruut_1_2_Changed);
+                mruut2.CheckedChanged += new EventHandler(Mruut_1_2_Changed);
+                this.Controls.Add(mruut1);
+                this.Controls.Add(mruut2);
+            }
+            else if (e.Node.Text == "Radionupp-Radiobutton")
+            {
+                rnupp1 = new RadioButton
+                {
+                    Text = "Paremale",
+                    Width = 112,
+                    Location=new Point(mruut2.Left+mruut2.Width, mruut1.Height + mruut2.Height)
+                };
+                rnupp2 = new RadioButton
+                {
+                    Text = "Vasakule",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width+rnupp1.Width, mruut1.Height + mruut2.Height)
+                };
+                rnupp3 = new RadioButton
+                {
+                    Text = "Ülesse",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width+rnupp2.Width+rnupp1.Width, mruut1.Height + mruut2.Height)
+                };
+                rnupp4 = new RadioButton
+                {
+                    Text = "Alla",
+                    Width = 112,
+                    Location = new Point(mruut2.Left + mruut2.Width+rnupp3.Width+rnupp1.Width+rnupp2.Width, mruut1.Height + mruut2.Height)
+                };
+                pilt = new PictureBox
+                {
+                    Image=new Bitmap("pgn.png"),
+                    Location=new Point(300,300),
+                    Size=new Size(100,100),
+                    SizeMode=PictureBoxSizeMode.Zoom
+                };
+                rnupp1.CheckedChanged += new EventHandler(sizepic);
+                rnupp2.CheckedChanged += new EventHandler(sizepic);
+                rnupp3.CheckedChanged += new EventHandler(sizepic);
+                rnupp4.CheckedChanged += new EventHandler(sizepic);
+                this.Controls.Add(rnupp1);
+                this.Controls.Add(rnupp2);
+                this.Controls.Add(rnupp3);
+                this.Controls.Add(rnupp4);
+                this.Controls.Add(pilt);
+            }
+            else if (e.Node.Text == "ProgressBar")
+            {
+                riba = new ProgressBar
+                {
+                    Value = 0,
+                    Minimum = 0,
+                    Maximum = 100,
+                    Step = 1,
+                    Dock = DockStyle.Bottom
+
+                };
+                aeg = new Timer();
+                aeg.Enabled = true;
+                aeg.Tick += Aeg_Tick;
+                this.Controls.Add(riba);
+            }
+            else if (e.Node.Text == "Mouse-button")
+            {
+                button4 = new Button();
+                button4.Text = "Klõpsake!";
+                button4.Height = 30;
+                button4.Width = 100;
+                button4.Location = new Point(340, 340);
+                button4.MouseMove += button4_MouseMove;
+                this.Controls.Add(button4);
+            }
         }
+
+        private void Aeg_Tick(object sender, EventArgs e)
+        {
+            riba.PerformStep();
+        }
+        int x = 300;
+        int y = 300;
+        private void sizepic(object sender, EventArgs e)
+        {
+            if (rnupp1.Checked)
+            {
+                x += 20;
+                pilt.Location = new Point(x, y);
+                rnupp1.Checked = false;
+            }
+            else if (rnupp2.Checked)
+            {
+                x -= 20;
+                pilt.Location = new Point(x, y);
+                rnupp2.Checked = false;
+            }
+            else if (rnupp3.Checked)
+            {
+                y -= 20;
+                pilt.Location = new Point(x, y);
+                rnupp3.Checked = false;
+            }
+            else if (rnupp4.Checked)
+            {
+                y += 20;
+                pilt.Location = new Point(x, y);
+                rnupp4.Checked = false;
+            }
+        }
+        private void button4_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Random x = new Random();
+            Random y = new Random();
+            int xx = Convert.ToInt32(x.Next(124, 300));
+            int yy = Convert.ToInt32(y.Next(35, 250));
+            button4.Location = new Point(xx, yy);
+
+        }
+        private void Mruut_1_2_Changed(object sender, EventArgs e)
+        {
+            if (mruut1.Checked == true && mruut2.Checked == true)
+            {
+                BackColor = Color.Green;
+                MessageBox.Show("Green","Color", 0, MessageBoxIcon.Hand);
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == true)
+            {
+                BackColor = Color.Yellow;
+                MessageBox.Show("Yellow", "Color", 0, MessageBoxIcon.Hand);
+            }
+            else if (mruut1.Checked == true && mruut2.Checked == false)
+            {
+                BackColor = Color.Blue;
+                MessageBox.Show("Blue", "Color", 0, MessageBoxIcon.Hand);
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == false)
+            {
+                BackColor = Color.LightSkyBlue;
+                MessageBox.Show("LightSkyBlue", "Color",0,MessageBoxIcon.Hand);
+            }
+        }
+        /*    private void Mruut2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mruut1.Checked == true && mruut2.Checked == true)
+            {
+
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == true)
+            {
+
+            }
+            else if (mruut1.Checked == true && mruut2.Checked == false)
+            {
+
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == false)
+            {
+
+            }
+        }
+
+        private void Mruut1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (mruut1.Checked==true && mruut2.Checked==true)
+            {
+                BackColor = Color.Green;
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == true)
+            {
+                BackColor = Color.Yellow;
+            }
+            else if (mruut1.Checked == true && mruut2.Checked == false)
+            {
+                BackColor = Color.Blue;
+            }
+            else if (mruut1.Checked == false && mruut2.Checked == false)
+            {
+                BackColor = Color.LightSkyBlue;
+            }
+        }*/
+
         public void Silt_MouseEnter(object sender, EventArgs e)
         {
             silt.ForeColor = Color.Black;
